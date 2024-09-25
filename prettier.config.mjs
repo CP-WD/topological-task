27352018ddb9b738bc0b8d80655043f9debcd089
Gavagai <77731060+CP-WD@users.noreply.github.com>
2024-09-26 01:02:17 +0900

    プロジェクトの設定を追加 (#2)
    * chore: Issue, PRのテンプレートを追加
    
    * chore: ESLint, Prettier関連の設定を追加

---
+++ prettier.config.mjs
@@ -0,0 +1,5 @@
+/** @type {import("prettier").Config} */
+export default {
+  // Handle this issue: https://www.reddit.com/r/vscode/comments/gbfrzo/eslint_adding_and_then_removing_my_trailing/
+  trailingComma: "none"
+};
