a96d8f9df654586a2c9e52335d66e3237d8686a5
CP-WD <hkana2912@outlook.com>
2024-09-27 03:04:00 +0900

    feat: データベースの基本的なCRUD操作

---
+++ src/utils/db/server/task.ts
@@ -0,0 +1,32 @@
+import { Task } from "~/types/Task";
+import { createClient } from "~/utils/db/supabase/client/server";
+
+const taskTableName = "task";
+
+export const insertTask = async (newTask: Task) => {
+  const supabase = createClient();
+  const { data: resTask } = await supabase.from(taskTableName).insert(newTask);
+
+  return resTask;
+};
+
+export const getAllTasks = async () => {
+  const supabase = createClient();
+  const { data: tasks } = await supabase.from(taskTableName).select();
+
+  return tasks;
+};
+
+export const updateTask = async (task: Task) => {
+  const supabase = createClient();
+  const { data: resTask } = await supabase.from(taskTableName).update(task).match({ id: task.id });
+
+  return resTask;
+};
+
+export const deleteTask = async (taskId: string) => {
+  const supabase = createClient();
+  const { data: resTask } = await supabase.from(taskTableName).delete().match({ id: taskId });
+
+  return resTask;
+};
