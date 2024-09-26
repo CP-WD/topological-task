90d00ebf7d14a8bd4313196d688336a962e8fbd6
CP-WD <hkana2912@outlook.com>
2024-09-27 01:51:01 +0900

    chore: Supabase用の型情報を追加

--- src/utils/db/supabase/client/server.ts
+++ src/utils/db/supabase/client/server.ts
@@ -1,11 +1,13 @@
 // https://supabase.com/docs/guides/auth/server-side/creating-a-client?queryGroups=framework&framework=nextjs&queryGroups=environment&environment=server
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { Database } from "~/utils/db/supabase/database.types";
 
 export const createClient = () => {
   const cookieStore = cookies();
 
-  return createServerClient(
+  // 型定義: https://supabase.com/docs/guides/api/rest/generating-types
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
