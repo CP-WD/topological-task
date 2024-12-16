dc314ab1027f6644f3faaab0c3c29892127188f5
CP-WD <hkana2912@outlook.com>
2024-12-16 13:28:03 +0900

    WIP: デバッグ用出力

--- src/utils/url/getWebBaseUrl.ts
+++ src/utils/url/getWebBaseUrl.ts
@@ -2,7 +2,9 @@ export const getWebBaseUrl = () => {
   console.log(process.env.VERCEL);
   console.log(process.env.VERCEL_URL);
   console.log(process.env.WEB_BASE_URL);
+  console.log(!!process.env.VERCEL);
   if (process.env.VERCEL) {
+    console.log("VERCEL");
     return process.env.VERCEL_URL;
   }
   return process.env.WEB_BASE_URL;
