c98cc7009b5d80578bace087a91e2ef95d6dc5ff
CP-WD <hkana2912@outlook.com>
2024-12-16 15:53:05 +0900

    refactor: デバッグ用コードの削除

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -8,8 +8,7 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  // const next = searchParams.get("next") ?? REDIRECT_ROUTE_AFTER_SIGN_IN;
-  const next = REDIRECT_ROUTE_AFTER_SIGN_IN;
+  const next = searchParams.get("next") ?? REDIRECT_ROUTE_AFTER_SIGN_IN;
 
   if (code) {
     const supabase = createClient();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
-import { getWebBaseUrl } from "~/utils/url/getWebBaseUrl";
 
 export default function Home() {
-  getWebBaseUrl();
   return (
     <div className="h-screen flex justify-center items-center">
       <Button asChild>
