9e0e0c93f74495dfda589e114280ba58740ff5de
CP-WD <hkana2912@outlook.com>
2024-12-16 13:21:32 +0900

    WIP: デバッグ用の関数呼び出し

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
+import { getWebBaseUrl } from "~/utils/url/getWebBaseUrl";
 
 export default function Home() {
+  getWebBaseUrl();
   return (
     <div className="h-screen flex justify-center items-center">
       <Button asChild>
