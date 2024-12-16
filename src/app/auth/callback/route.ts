d355f7eb0b3bfda09b4ddc131610581205f6bbc5
CP-WD <hkana2912@outlook.com>
2024-12-16 14:38:30 +0900

    WIP: デバッグ用の出力

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -9,10 +9,15 @@ export async function GET(request: Request) {
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? REDIRECT_ROUTE_AFTER_SIGN_IN;
+  console.log("code", code);
+  console.log("next", next);
+  console.log("origin", origin);
+  console.log("next", next);
 
   if (code) {
     const supabase = createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+    console.log("error", error);
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
