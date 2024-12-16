6541b7f918538d2d1c98e2a183198b21d0fc2828
CP-WD <hkana2912@outlook.com>
2024-12-16 14:49:43 +0900

    WIP: リダイレクト先の変更

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -8,7 +8,8 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? REDIRECT_ROUTE_AFTER_SIGN_IN;
+  // const next = searchParams.get("next") ?? REDIRECT_ROUTE_AFTER_SIGN_IN;
+  const next = REDIRECT_ROUTE_AFTER_SIGN_IN;
   console.log("code", code);
   console.log("next", next);
   console.log("origin", origin);
