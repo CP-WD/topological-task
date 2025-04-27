2ba42f93a5ad711fa2476bdfd89a45719e1564fb
CP-WD <hkana2912@outlook.com>
2025-04-27 14:26:38 +0900

    feat: トポロジカルソートの結果を表示するページ

--- src/app/task/[id]/graph/page.tsx
+++ src/app/task/[id]/graph/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 import { Graph } from "~/components/Graph/Graph";
+import { Button } from "~/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { getAllRelationEdge } from "~/utils/db/server/relationEdge";
 import { getAllTasks } from "~/utils/db/server/task";
 import { genGraphDefinition } from "~/utils/graph/cytoscape/genGraphDefinition";
+import { canTopologicallySort } from "~/utils/graph/topologicalSort";
 
 const fetchData = async () => {
   const tasks = await getAllTasks();
@@ -25,30 +28,39 @@ const Page = async ({ params: { id } }: { params: { id: string } }) => {
   const containmentElements = genGraphDefinition(id, tasks, containmentEdges);
   const dependencyElements = genGraphDefinition(id, tasks, dependencyEdges);
   const allElements = genGraphDefinition(id, tasks, [...containmentEdges, ...dependencyEdges]);
+  const canSort = canTopologicallySort([...dependencyEdges], id);
 
   return (
-    <Tabs defaultValue="containment" className="">
-      <TabsList className="flex justify-start">
-        <TabsTrigger value="containment">Containment</TabsTrigger>
-        <TabsTrigger value="dependency">Dependency</TabsTrigger>
-        <TabsTrigger value="all">All</TabsTrigger>
-      </TabsList>
-      <TabsContent value="containment">
-        <div className="h-[400px]">
-          <Graph elements={containmentElements} />
-        </div>
-      </TabsContent>
-      <TabsContent value="dependency">
-        <div className="h-[400px]">
-          <Graph elements={dependencyElements} />
-        </div>
-      </TabsContent>
-      <TabsContent value="all">
-        <div className="h-[400px]">
-          <Graph elements={allElements} />
-        </div>
-      </TabsContent>
-    </Tabs>
+    <>
+      <Tabs defaultValue="containment" className="">
+        <TabsList className="flex justify-start">
+          <TabsTrigger value="containment">Containment</TabsTrigger>
+          <TabsTrigger value="dependency">Dependency</TabsTrigger>
+          <TabsTrigger value="all">All</TabsTrigger>
+        </TabsList>
+        <TabsContent value="containment">
+          <div className="h-[400px]">
+            <Graph elements={containmentElements} />
+          </div>
+        </TabsContent>
+        <TabsContent value="dependency">
+          <div className="h-[400px]">
+            <Graph elements={dependencyElements} />
+          </div>
+        </TabsContent>
+        <TabsContent value="all">
+          <div className="h-[400px]">
+            <Graph elements={allElements} />
+          </div>
+        </TabsContent>
+      </Tabs>
+
+      <p>
+        <Button asChild disabled={!canSort}>
+          <Link href={`/task/${id}/sort`}>view topological sort</Link>
+        </Button>
+      </p>
+    </>
   );
 };
 
